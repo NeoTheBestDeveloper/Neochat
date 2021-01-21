b7b303dbd641ecebaafa6d695ee93f6170936c57
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-01-21 18:28:11 +0300

    Updates

---
+++ precache-manifest.261d0c8f3547678238823378993154a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd57d0dd67e1953fdea31688985a8ff3",
+    "url": "/Neochat/index.html"
+  },
+  {
+    "revision": "192a4940cf9d74e04010",
+    "url": "/Neochat/static/css/main.e490fba8.chunk.css"
+  },
+  {
+    "revision": "6400177e76d004289ca9",
+    "url": "/Neochat/static/js/2.a8a58b31.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Neochat/static/js/2.a8a58b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192a4940cf9d74e04010",
+    "url": "/Neochat/static/js/main.4f5450e3.chunk.js"
+  },
+  {
+    "revision": "526e653ad884e0099bcb",
+    "url": "/Neochat/static/js/runtime-main.47528630.js"
+  },
+  {
+    "revision": "a575ad6a7212b2a2c454b49b6bfa6ba9",
+    "url": "/Neochat/static/media/30.a575ad6a.jpg"
+  },
+  {
+    "revision": "8a9519e1b71bb160c25d3ecdef4189ea",
+    "url": "/Neochat/static/media/7.8a9519e1.jpg"
+  },
+  {
+    "revision": "f9d5315ad29eb81c87690ccc0ddc26e6",
+    "url": "/Neochat/static/media/avaSvgDefault.f9d5315a.svg"
+  },
+  {
+    "revision": "d28f6a894893108af96b6c0487fe1302",
+    "url": "/Neochat/static/media/thumbs-down-regular.d28f6a89.svg"
+  },
+  {
+    "revision": "f1faec9fd7d73647cd27d2879a6d1a47",
+    "url": "/Neochat/static/media/thumbs-down-solid.f1faec9f.svg"
+  },
+  {
+    "revision": "d51a5b468e8a36fc7c2d02c9e97dbc6e",
+    "url": "/Neochat/static/media/thumbs-up-regular.d51a5b46.svg"
+  },
+  {
+    "revision": "dbeab264222432d7ee7f09859bf8490e",
+    "url": "/Neochat/static/media/thumbs-up-solid.dbeab264.svg"
+  },
+  {
+    "revision": "5e946c20186e33b608fbb3388fff6ff5",
+    "url": "/Neochat/static/media/user.5e946c20.svg"
+  }
+]);
